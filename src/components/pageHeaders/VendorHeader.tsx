7e07deb54ec30bf39c9dee6c33a8fb30fc52faab
9raja1999 <draj29209@gmail.com>
2023-09-29 06:27:03 -0700

    add vendor form

--- src/components/pageHeaders/VendorHeader.tsx
+++ src/components/pageHeaders/VendorHeader.tsx
@@ -1,22 +1,31 @@
+'use client'
 
+import { useState, useCallback } from 'react'
 import {
     Select
 } from 'antd'
 import { DropdDownIcon, FilterIcon } from '@/utils/arrowIcons'
 import { AddCircularIcon } from '@/utils/buttonIcons'
 import ExportBtn from '../buttons/dataBtns/ExportBtn'
+import AddVendorForm from '../forms/AddVendorForm'
+import DrawerLayout from '../drawers/DrawerLayout'
 import styles from '../../../styles/components/headers/page.header.module.scss'
 
 export default function VendorHeader() {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const handleClose = useCallback(() => setOpen(false), [open])
+
+
     return (
         <div className={styles.insightsHeader}>
             <div className={styles.pageInfo}>
                 <h1 className={styles.pageName}>Vendors</h1>
             </div>
             <div className={styles.actions}>
-                <button className={styles.addBtn}>
+                <button onClick={() => setOpen(true)} className={styles.addBtn}>
                     <AddCircularIcon />
-                    <span>Add new inventory</span>
+                    <span>Add new vendor</span>
                 </button>
                 <button className={styles.filterBtn}>
                     <FilterIcon />
@@ -34,6 +43,14 @@ export default function VendorHeader() {
                 </button>
                 <ExportBtn text="Export List" />
             </div>
+
+            {/* Add vendor drawer */}
+            <DrawerLayout
+                open={open}
+                onClose={handleClose}
+            >
+               <AddVendorForm />
+            </DrawerLayout>
         </div>
     )
 }
